8663e6453a0a7879924c6e003d42ca319395a905
SheenM <sheenm@ya.ru>
2018-05-25 22:28:13 +1000

    added ServiceResponse

---
+++ NewBuildings.Core/ResponseStatuses.cs
@@ -0,0 +1,9 @@
+﻿namespace NewBuildings.Core
+{
+    public enum ResponseStatuses
+    {
+        Ok = 0,
+        Warning = 1,
+        Exception = 2
+    }
+}
---
+++ NewBuildings.Core/ServiceResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace NewBuildings.Core
+{
+    public class ServiceResponse<T>
+    {
+        public ResponseStatuses Status { get; set; }
+
+        public T Data { get; set; }
+
+        public string Message { get; set; }
+
+        public static ServiceResponse<T> Ok(T data, string message = "")
+        {
+            return new ServiceResponse<T>
+            {
+                Data = data,
+                Message = message,
+                Status = ResponseStatuses.Ok
+            };
+        }
+
+        public static ServiceResponse<T> Warning(string message, T data = default(T))
+        {
+            return new ServiceResponse<T>
+            {
+                Data = data,
+                Message = message,
+                Status = ResponseStatuses.Warning
+            };
+        }
+
+        public static ServiceResponse<T> Exception(string message)
+        {
+            return new ServiceResponse<T>
+            {
+                Data = default(T),
+                Message = message,
+                Status = ResponseStatuses.Exception
+            };
+        }
+    }
+}
