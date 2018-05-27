e7aa6215fde9ac463518f5b9ddea1403dabed968
SheenM <sheenm@ya.ru>
2018-05-27 14:02:56 +1000

    added font-awesome icons

--- src/NewBuildings.Web/ClientApp/src/main.ts
+++ src/NewBuildings.Web/ClientApp/src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 
+import fontawesome from '@fortawesome/fontawesome'
+import * as faEdit from '@fortawesome/fontawesome-free-regular/faEdit'
+import * as faView from '@fortawesome/fontawesome-free-regular/faEye'
+import * as faTrashAlt from '@fortawesome/fontawesome-free-regular/faTrashAlt'
+
+fontawesome.library.add(faEdit)
+fontawesome.library.add(faView)
+fontawesome.library.add(faTrashAlt)
+
 export function getBaseUrl() {
     return document.getElementsByTagName('base')[0].href
 }
